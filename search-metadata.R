8ab778aa74f53050dd03e4f9668a95e1b85aad65
Zebulun Arendsee <zbwrnz@gmail.com>
2016-04-04 03:24:40 -0500

    Add study ids

--- search-metadata.R
+++ search-metadata.R
@@ -11,7 +11,7 @@ sra_con <- dbConnect(SQLite(),sqlfile)
 
 tables <- function() dbListTables(sra_con)
 fields <- function(table) dbListFields(sra_con, table)
-query  <- function(cmd) dbGetQuery(src_con, cmd)
+query  <- function(cmd) dbGetQuery(conn=sra_con, statement=cmd)
 
 fetch <- function(fields, table, condition, limit){
   fields = paste(fields, collapse=", ")
@@ -23,12 +23,34 @@ fetch <- function(fields, table, condition, limit){
   if(!missing(limit)){
     cmd=sprintf("%s limit %d", cmd, limit) 
   }
-  dbGetQuery(sra_con, cmd)
+  dbGetQuery(conn=sra_con, statement=cmd)
 }
+
+stuid = c(
+    'SRP009850', # shoot apical meristems stages
+    'SRP018034', # leaf senesence stages
+    'SRP053394', # hi/lo temp
+    'SRP059384', # roots versus shoots
+    'SRP059724', # (hi/lo Pi) X (mock/Ct/Ci) X (6,10,16,24 dpi)
+    'SRP063314', # leaf, flower, root
+    'SRP064782', # circadian clock ???
+    'SRP063421', # (hi/lo Pi) X (root, shoot) X (long, short exposure)
+    'SRP069266'  # high altitude adaptation
+)
+
+fjoin <- function(x) sprintf("'%s'", paste0(x, collapse="', '"))
+
+getRuns <- function(stuid){
+  sprintf("%s", paste0(stuid, collapse="', '")) %>%
+    sprintf(fmt="select * from sra_ft where study_accession in ( %s )") %>%
+    query()
+}
+
+runs <- getRuns(stuid)[c()]
+
 
 # taxon_id:3702 library_strategy:RNA library_source:TRANSCRIPTOMIC library_layout:PAIRED
 
-fetch("abstract", "study", "taxon_id = 3702", 5)
 
 # fetch(
 #   fields    = "study_title",
@@ -56,4 +78,8 @@ fetch("abstract", "study", "taxon_id = 3702", 5)
 
 # For each, fetch | fastq-dump | kallisto
 
-# Merge all into final expression table
+# ---------
+
+# In Sleuth, normalize all the studies
+# merge into final expression table
+# 
