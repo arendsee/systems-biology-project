68769940a7e089413e52e0deac604954a8b305a0
Zebulun Arendsee <zbwrnz@gmail.com>
2016-04-24 22:26:34 -0500

    Add help

--- kallistoize-all-runids.sh
+++ kallistoize-all-runids.sh
@@ -1,3 +1,22 @@
+#!/usr/bin/env bash
+
+usage (){
+cat << EOF
+This is a wrapper for process-sra.sh.
+Requires 1 argument: a file of SRA runids
+EOF
+    exit 0
+}
+
+while getopts "h" opt; do
+    case $opt in
+        h)
+            usage ;;
+    esac 
+done
+
+[[ -z $1 ]] && usage
+
 while read id
 do
     echo $id
