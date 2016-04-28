a38c717b4e6917a9fec002eb15c30f7390ce3d11
Zebulun Arendsee <zbwrnz@gmail.com>
2016-04-28 08:18:36 -0500

    Print network stats ar each cutoff

--- find-tfs.sh
+++ find-tfs.sh
@@ -22,7 +22,9 @@ tfs     <- intersect(tf.info$locus, rownames(em))
 
 loci <- Reduce(union, list(rownames(em), orphans , tf.info$locus))
 
-    loci <- Reduce(union, list(orphans, tf.info$locus, sample(rownames(em), 1000)))
+    long.tf.list <- read.delim('long-tf-list.tab')$gene_model %>%
+        gsub(pattern='\\.\\d+', replacement='') %>% unique
+    loci <- Reduce(union, list(orphans, tf.info$locus, long.tf.list, sample(rownames(em), 2000)))
 
 em <- em[loci, ]
 em[is.na(em)] <- 0
@@ -31,7 +33,7 @@ em[is.na(em)] <- 0
 mim <- build.mim(t(em), estimator="spearman")
 aracne.net <- aracne(mim)
 
-others  <- setdiff(loci, c(orphans, tfs))
+others  <- setdiff(loci, c(orphans, tfs, long.tf.list))
 
 testset <- list(
     orphans = orphans,
@@ -44,13 +46,28 @@ testset <- list(
 
 
 cutoffs <- seq(from=0.5, to=0.975, by=0.025)
-for (cut in cutoffs){
-  dname <- file.path(output, paste0('c', cut))
+g.degrees  = list()
+g.stats <- data.frame(
+    cutoff   = cutoffs,
+    avg_path = rep(0, length(cutoffs)),
+    density  = rep(0, length(cutoffs))
+)
+for (i in 1:length(cutoff)){
+  dname <- file.path(output, paste0('c', cut[i]))
   dir.create(dname, recursive=TRUE, showWarnings=FALSE)
-  ag <- adj2net(aracne.net > cut)
+  ag <- adj2net(aracne.net > cut[i])
+  g.stats[i, 'avg_path'] <- mean_distance(ag)
+  g.stats[i, 'density'] <- graph.density(ag)
+  g.degrees[[i ]] <- data.frame(degree=degree(g)) 
+  g.degrees[[i ]]$cutoff <- cut
   for (n in names(testset)){
       which(!vertex_attr(ag)$name %in% c(tfs, testset[[n]])) %>%
           delete_vertices(graph=ag) %>%
           write.graph(file.path(dname, paste0(n, '.mat')), 'ncol')
   }
 }
+
+g.degrees <- Reduce(rbind, g.degrees)
+write.table(g.degrees, file.path(output, 'graph-degrees.tab'), sep="\t", row.names=FALSE)
+
+write.table(g.stats, file.path(output, 'graph-stats.tab'), sep="\t", row.names=FALSE)
